f02eaf9de9837d5a73ff44bba1aff6e38bcff420
123 <dingrb52240@163.com>
2024-12-23 21:53:51 +0800

    test2

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,6 +2,9 @@ public class test {
    public static void main(String[] args) {
 
        System.out.println("Hello World");
+       System.out.println("Hello World");
+       System.out.println("Hello World");
+
    }
 
 }
